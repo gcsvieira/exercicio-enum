cda8f516cb7d731e304fcecc475af9ce586cab5c
gcsvieira <74613475+gcsvieira@users.noreply.github.com>
2022-11-05 17:05:30 -0300

    first commit

---
+++ src/entities/Client.java
@@ -0,0 +1,42 @@
+package entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Client {
+    DateTimeFormatter standardFmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    private String name;
+    private String email;
+    private LocalDate birthDate;
+
+    public Client(String name, String email, LocalDate birthDate) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return this.birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
